678b2ad46cf39e1bac8ca4be26b70dcb936f93e4
Graeme Hill <graemekh@gmail.com>
2019-06-27 09:08:44 -0700

    Basic schema/model building

--- schema.go
+++ schema.go
@@ -1,6 +1,107 @@
 package main
 
+import "fmt"
+
+type Model struct {
+	Tables map[string]*Table
+}
+
 type Table struct {
-	Name string
+	Name    string
 	Columns []ColumnDefinition
+}
+
+type ModelBuilder struct {
+	model Model
+}
+
+func NewModelBuilder() *ModelBuilder {
+	return &ModelBuilder{
+		model: Model{
+			Tables: map[string]*Table{},
+		},
+	}
+}
+
+func (m *ModelBuilder) handleStmt(stmt Statement) error {
+	switch s := stmt.(type) {
+	case CreateTable:
+		return m.handleCreateTableStmt(s)
+	case AddColumn:
+		return m.handleAddColumnStmt(s)
+	case DropColumn:
+		return m.handleDropColumnStmt(s)
+	default:
+		// Ignore because we don't care about SELECT, INSERT, etc
+		return nil
+	}
+}
+
+func (m *ModelBuilder) handleCreateTableStmt(ct CreateTable) error {
+	_, exists := m.model.Tables[ct.Name]
+	if exists {
+		return fmt.Errorf("Table named '%s' already exists", ct.Name)
+	}
+
+	tbl := &Table{
+		Name:    ct.Name,
+		Columns: ct.Columns,
+	}
+
+	m.model.Tables[tbl.Name] = tbl
+	return nil
+}
+
+func (m *ModelBuilder) handleAddColumnStmt(ac AddColumn) error {
+	tbl, tblExists := m.model.Tables[ac.TableName]
+	if !tblExists {
+		return fmt.Errorf(
+			"Cannot add column '%s' because the table '%s' does not exist",
+			ac.Column.Name,
+			ac.TableName,
+		)
+	}
+
+	for _, col := range tbl.Columns {
+		if col.Name == ac.Column.Name {
+			return fmt.Errorf(
+				"Cannot add column '%s' to the table '%s' because the column already exists",
+				ac.Column.Name,
+				ac.TableName,
+			)
+		}
+	}
+
+	tbl.Columns = append(tbl.Columns, ac.Column)
+	return nil
+}
+
+func (m *ModelBuilder) handleDropColumnStmt(dc DropColumn) error {
+	tbl, tblExists := m.model.Tables[dc.TableName]
+	if !tblExists {
+		return fmt.Errorf(
+			"Cannot drop column '%s' because the table '%s' does not exist",
+			dc.ColumnName,
+			dc.TableName,
+		)
+	}
+
+	toRemove := -1
+	for i, col := range tbl.Columns {
+		if col.Name == dc.ColumnName {
+			toRemove = i
+			break
+		}
+	}
+
+	if toRemove < 0 {
+		return fmt.Errorf(
+			"Cannot drop column '%s' from the table '%s' because the column does not exist",
+			dc.ColumnName,
+			dc.TableName,
+		)
+	}
+
+	tbl.Columns = append(tbl.Columns[:toRemove], tbl.Columns[toRemove+1:]...)
+	return nil
 }
---
+++ schema_test.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestModelBuilder(t *testing.T) {
+	b := NewModelBuilder()
+
+	err := b.handleStmt(CreateTable{
+		Name: "users",
+		Columns: []ColumnDefinition{
+			ColumnDefinition{Name: "id"},
+			ColumnDefinition{Name: "name"},
+		},
+	})
+
+	require.NoError(t, err)
+	require.Len(t, b.model.Tables, 1)
+	require.Len(t, b.model.Tables["users"].Columns, 2)
+	require.Equal(t, "id", b.model.Tables["users"].Columns[0].Name)
+	require.Equal(t, "name", b.model.Tables["users"].Columns[1].Name)
+
+	err = b.handleStmt(AddColumn{
+		TableName: "users",
+		Column:    ColumnDefinition{Name: "email"},
+	})
+
+	require.NoError(t, err)
+	require.Len(t, b.model.Tables, 1)
+	require.Len(t, b.model.Tables["users"].Columns, 3)
+	require.Equal(t, "id", b.model.Tables["users"].Columns[0].Name)
+	require.Equal(t, "name", b.model.Tables["users"].Columns[1].Name)
+	require.Equal(t, "email", b.model.Tables["users"].Columns[2].Name)
+
+	err = b.handleStmt(DropColumn{
+		TableName:  "users",
+		ColumnName: "name",
+	})
+
+	require.NoError(t, err)
+	require.Len(t, b.model.Tables, 1)
+	require.Len(t, b.model.Tables["users"].Columns, 2)
+	require.Equal(t, "id", b.model.Tables["users"].Columns[0].Name)
+	require.Equal(t, "email", b.model.Tables["users"].Columns[1].Name)
+}
+
+func TestModelBuilderDropColumnFail(t *testing.T) {
+	b := NewModelBuilder()
+
+	err := b.handleStmt(CreateTable{
+		Name: "users",
+		Columns: []ColumnDefinition{
+			ColumnDefinition{Name: "id"},
+			ColumnDefinition{Name: "name"},
+		},
+	})
+	require.NoError(t, err)
+
+	err = b.handleStmt(DropColumn{
+		TableName:  "users",
+		ColumnName: "doesnotexist",
+	})
+	require.Error(t, err)
+
+	err = b.handleStmt(DropColumn{
+		TableName:  "doesnotexist",
+		ColumnName: "name",
+	})
+	require.Error(t, err)
+}
+
+func TestModelBuilderCreateTableFail(t *testing.T) {
+	b := NewModelBuilder()
+
+	err := b.handleStmt(CreateTable{
+		Name: "users",
+		Columns: []ColumnDefinition{
+			ColumnDefinition{Name: "id"},
+			ColumnDefinition{Name: "name"},
+		},
+	})
+	require.NoError(t, err)
+
+	err = b.handleStmt(CreateTable{
+		Name: "users",
+		Columns: []ColumnDefinition{
+			ColumnDefinition{Name: "id2"},
+			ColumnDefinition{Name: "name2"},
+		},
+	})
+	require.Error(t, err)
+}
+
+func TestModelBuilderAddColumnFail(t *testing.T) {
+	b := NewModelBuilder()
+
+	err := b.handleStmt(CreateTable{
+		Name: "users",
+		Columns: []ColumnDefinition{
+			ColumnDefinition{Name: "id"},
+			ColumnDefinition{Name: "name"},
+		},
+	})
+	require.NoError(t, err)
+
+	err = b.handleStmt(AddColumn{
+		TableName: "users",
+		Column:    ColumnDefinition{Name: "name"},
+	})
+	require.Error(t, err)
+}
