3afd1fcc20ac094fe67c690e4cc6d661a857741e
Graeme Hill <graemekh@gmail.com>
2019-07-07 11:17:11 -0700

    Fix e2e tests
    They pass now but they are still not totally end to end

--- test/e2e_test.go
+++ test/e2e_test.go
@@ -1,14 +1,21 @@
 package test
 
 import (
+	"context"
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/graeme-hill/sqlstuff-go/lib"
 	"github.com/graeme-hill/sqlstuff-go/test/store"
+	"github.com/stretchr/testify/require"
 )
 
 func TestAll(t *testing.T) {
-	client, err := store.NewDBClient("dbname=graeme")
+	connStr := "user=postgres password=password"
+	client, err := store.NewDBClient(connStr)
+	require.NoError(t, err)
+
+	ctx := context.Background()
+	err = lib.RunMigrations(ctx, "./migrations", connStr)
 	require.NoError(t, err)
 
 	users, err := client.GetUsers()
@@ -20,5 +27,5 @@ func TestAll(t *testing.T) {
 	require.Equal(t, "Hill", users[0].LastName)
 
 	require.Equal(t, "Graeme", users[1].FirstName)
-	require.Equal(t, "Hill", users[2].LastName)
+	require.Equal(t, "Hill", users[1].LastName)
 }
